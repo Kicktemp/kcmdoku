384ba0e8cb25e675281f92689447c5d56a7d5888
Niels Nübel <niels@kicktemp.com>
2020-06-19 08:48:09 +0200

    Update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,91 @@
 module.exports = {
-  title: 'Hello VuePres2s',
-  description: 'Just playing around'
+  title: 'Kick Consent Manager',
+  locales: {
+    '/': {
+      lang: 'de-DE',
+      title: 'Kick Consent Manager',
+      description: 'Cookie Consent Manager für Joomla'
+    },
+    '/en/': {
+      lang: 'en-GB',
+      title: 'Kick Consent Manager',
+      description: 'Cookie Consent Manager for Joomla'
+    }
+  },
+  head: [
+    ['link', { rel: 'icon', href: `/logo.png` }]
+  ],
+  themeConfig: {
+    repo: 'Kicktemp/kcmdoku',
+    editLinks: true,
+    smoothScroll: true,
+    locales: {
+      '/': {
+        label: 'Deutsch',
+        selectText: 'Sprachen',
+        ariaLabel: 'Sprachen',
+        editLinkText: 'Diese Seite auf GitHub bearbeiten',
+        lastUpdated: 'Zuletzt aktualisiert',
+        serviceWorker: {
+          updatePopup: {
+            message: "Neuer Inhalt verfügbar.",
+            buttonText: "Neuladen"
+          }
+        },
+        nav: require('./nav/de'),
+        sidebar: {
+          '/guide/': getGuideSidebar('Guide', 'Advanced'),
+        }
+      },
+      '/en/': {
+        selectText: 'Languages',
+        label: 'English',
+        ariaLabel: 'Languages',
+        editLinkText: 'Edit this page on GitHub',
+        lastUpdated: 'Last Updated',
+        serviceWorker: {
+          updatePopup: {
+            message: "New content is available.",
+            buttonText: "Refresh"
+          }
+        },
+      }
+    }
+  },
+  plugins: [
+    ['@vuepress/back-to-top', true],
+  ],
+  extraWatchFiles: [
+    '.vuepress/nav/de.js'
+  ]
+}
+
+function getGuideSidebar (groupA, groupB) {
+  return [
+    {
+      title: groupA,
+      collapsable: false,
+      children: [
+        '',
+        'getting-started',
+        'directory-structure',
+        'basic-config',
+        'assets',
+        'markdown',
+        'using-vue',
+        'i18n',
+        'deploy'
+      ]
+    },
+    {
+      title: groupB,
+      collapsable: false,
+      children: [
+        'frontmatter',
+        'permalinks',
+        'markdown-slot',
+        'global-computed'
+      ]
+    }
+  ]
 }
--- docs/README.md
+++ docs/README.md
@@ -1,1 +1,45 @@
-# Hello VuePress
+---
+home: true
+heroImage: /kickconsentmanager_box.png
+actionText: Get Started →
+actionLink: /guide/
+footer: GNU Licensed | Copyright © 2020 Kicktemp GmbH. All rights reserved.
+---
+
+<div class="features">
+  <div class="feature">
+    <h2>Simplicity First</h2>
+    <p>Minimal setup with markdown-centered project structure helps you focus on writing.</p>
+  </div>
+  <div class="feature">
+    <h2>Vue-Powered</h2>
+    <p>Enjoy the dev experience of Vue + webpack, use Vue components in markdown, and develop custom themes with Vue.</p>
+  </div>
+  <div class="feature">
+    <h2>Performant</h2>
+    <p>VuePress generates pre-rendered static HTML for each page, and runs as an SPA once a page is loaded.</p>
+  </div>
+</div>
+
+### As Easy as 1, 2, 3
+
+``` bash
+# Scaffold VuePress site
+yarn create vuepress [directoryName]
+# OR npx create-vuepress [directoryName]
+
+# Navigate to your new VuePress directory
+cd [directoryName]
+
+# Start local dev server
+yarn dev
+# OR npm run dev
+
+# Build static files
+yarn build
+# OR npm run build
+```
+
+::: warning COMPATIBILITY NOTE
+Kick Consent Manager requires Joomla >= 3.8 and PHP >= 7.0.
+:::
